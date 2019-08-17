03ab65332c33ae9630e58f98250be850703113ff
Johan Sebastián Hernández <johan.hernandez@correounivalle.edu.co>
2019-08-17 12:58:57 -0500

    Add preview functionality to uploaded images

--- client/src/components/dropzone/dropzone.jsx
+++ client/src/components/dropzone/dropzone.jsx
@@ -1,125 +1,143 @@
-import React, { useState } from "react";
-import "./dropzone.css";
+import React, { useState, useEffect, useMemo } from "react";
+import { useDropzone } from "react-dropzone";
 import icon from "../../assets/images/baseline-cloud_upload-24px.svg";
 
+const baseStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+  icon: {
+    opacity: "0.3",
+    height: "64px",
+    width: "64px"
+  }
+};
+
+const activeStyle = {
+  borderColor: "#2196f3"
+};
+
+const acceptStyle = {
+  borderColor: "#00e676"
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744"
+};
+
+const thumbsStyles = {
+  thumbsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 16
+  },
+  thumb: {
+    display: "inline-flex",
+    borderRadius: 2,
+    border: "1px solid #eaeaea",
+    marginBottom: 8,
+    marginRight: 8,
+    width: 100,
+    height: 100,
+    padding: 4,
+    boxSizing: "border-box"
+  },
+  thumbInner: {
+    display: "flex",
+    minWidth: 0,
+    overflow: "hidden"
+  },
+  img: {
+    display: "block",
+    width: "auto",
+    height: "100%"
+  }
+};
+
 const Dropzone = props => {
-  const [hightlight, setHightlight] = useState(false);
-  const [imgSrc, setImgSrc] = useState("");
-  const fileInputRef = React.createRef();
+  const maxSize = 5242880;
+  const [files, setFiles] = useState([]);
 
-  // this.openFileDialog = this.openFileDialog.bind(this)
-  // this.onFilesAdded = this.onFilesAdded.bind(this)
-  // this.onDragOver = this.onDragOver.bind(this)
-  // this.onDragLeave = this.onDragLeave.bind(this)
-  // this.onDrop = this.onDrop.bind(this)
+  const {
+    isDragActive,
+    getRootProps,
+    getInputProps,
+    isDragAccept,
+    isDragReject,
+    rejectedFiles
+  } = useDropzone({
+    onDrop: acceptedFiles => {
+      setFiles(
+        acceptedFiles.map(file =>
+          Object.assign(file, { preview: URL.createObjectURL(file) })
+        )
+      );
+    },
+    accept: "image/*",
+    minSize: 0,
+    maxSize
+  });
 
-  const openFileDialog = () => {
-    if (props.disabled) return;
-    fileInputRef.current.click();
-  };
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {})
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  );
 
-  const onFilesAdded = evt => {
-    if (props.disabled) return;
-    const files = evt.target.files;
-    let filteredFiles = [];
-    let reader = new FileReader();
-    for (let i = 0; i < files.length; i++) {
-      switch (files[i].type) {
-        case "image/png":
-          filteredFiles.push(files[i]);
-          break;
-        case "image/jpg":
-          filteredFiles.push(files[i]);
-          break;
-        case "image/jpeg":
-          filteredFiles.push(files[i]);
-          break;
-        default:
-          alert(
-            "Unsupported file format: available formats are .png, .jpg, .jpeg"
-          );
-      }
-    }
+  const isFileTooLarge =
+    rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
 
-    if (props.onFilesAdded) {
-      const array = fileListToArray(filteredFiles);
-      props.onFilesAdded(array);
-    }
-    let url = reader.readAsDataURL(filteredFiles[0]);
-    reader.onloadend = function(e) {
-      setImgSrc(reader.result);
-    };
-  };
+  const thumbs = files.map(file => (
+    <div style={thumbsStyles.thumb} key={file.name}>
+      <div style={thumbsStyles.thumbInner}>
+        <img alt="preview" src={file.preview} style={thumbsStyles.img} />
+      </div>
+    </div>
+  ));
 
-  const onDragOver = evt => {
-    evt.preventDefault();
-
-    if (props.disabled) return;
-
-    setHightlight({ hightlight: true });
-  };
-
-  const onDragLeave = () => {
-    setHightlight({ hightlight: false });
-  };
-
-  const onDrop = event => {
-    event.preventDefault();
-
-    if (props.disabled) return;
-
-    const files = event.dataTransfer.files;
-    let filteredFiles = [];
-    for (let i = 0; i < files.length; i++) {
-      switch (files[i].type) {
-        case "image/png":
-          filteredFiles.push(files[i]);
-          break;
-        case "image/jpg":
-          filteredFiles.push(files[i]);
-          break;
-        case "image/jpeg":
-          filteredFiles.push(files[i]);
-          break;
-        default:
-          alert(
-            "Unsupported file format: available formats are .png, .jpg, .jpeg"
-          );
-      }
-    }
-    if (props.onFilesAdded) {
-      const array = fileListToArray(filteredFiles);
-      props.onFilesAdded(array);
-    }
-    setHightlight({ hightlight: false });
-  };
-
-  const fileListToArray = list => {
-    const array = [];
-    for (let i = 0; i < list.length; i++) {
-      array.push(list[i]);
-    }
-    return array;
-  };
+  useEffect(
+    () => () => {
+      // Revoke the data uris to avoid memory leaks
+      files.forEach(file => URL.revokeObjectURL(file.preview));
+    },
+    [files]
+  );
 
   return (
-    <div
-      className={`Dropzone ${hightlight ? "Highlight" : ""}`}
-      onDragOver={onDragOver}
-      onDragLeave={onDragLeave}
-      onDrop={onDrop}
-      onClick={openFileDialog}
-      style={{ cursor: props.disabled ? "default" : "pointer" }}
-    >
-      <input
-        ref={fileInputRef}
-        className="FileInput"
-        type="file"
-        multiple
-        onChange={onFilesAdded}
-      />
-      <img alt="upload" className="Icon" src={icon} />
-    </div>
+    <section className="container">
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+
+        {!isDragActive && (
+          <img
+            alt="upload"
+            style={baseStyle.icon}
+            src={icon}
+            title="Click here or drop a file to upload!"
+          />
+        )}
+        {isDragReject && "File type not accepted, sorry!"}
+        {isFileTooLarge && (
+          <div className="text-danger mt-2">File is too large.</div>
+        )}
+      </div>
+      <aside style={thumbsStyles.thumbsContainer}>{thumbs}</aside>
+    </section>
   );
 };
 
