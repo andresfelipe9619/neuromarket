b4c6639ca670bc14ed53b1c3d8fa4f4946974169
Andres Suarez <andresfelipe9619@gmail.com>
2019-09-01 21:45:52 -0500

    Make some changes on the models

--- server/models/order.js
+++ server/models/order.js
@@ -5,7 +5,12 @@ let OrderSchema = new Schema(
   {
     address: { type: Schema.Types.ObjectId, ref: "address" },
     user: { type: Schema.Types.ObjectId, ref: "user" },
-    products: [{ type: Schema.Types.ObjectId, ref: "product" }]
+    products: [
+      {
+        productId: { type: Schema.Types.ObjectId, ref: "product" },
+        quantity: Number
+      }
+    ]
   },
   { timestamps: true }
 );
--- server/models/user.js
+++ server/models/user.js
@@ -1,69 +1,64 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 let rolesValidos = {
-    values: ['ADMIN_ROLE', 'USER_ROLE'],
-    message: '{VALUE} no es un rol válido'
+  values: ["ADMIN_ROLE", "USER_ROLE"],
+  message: "{VALUE} no es un rol válido"
 };
-
 
 let Schema = mongoose.Schema;
 
-
 let usuarioSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'El name es necesario']
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: [true, 'El correo es necesario']
-    },
-    password: {
-        type: String,
-        required: [true, 'La contraseña es obligatoria']
-    },
-    phone:{
-        type: String,
-        requiered:[true, 'El telefono es requerido']
-    },    
-    img: {
-        type: String,
-        required: false
-    },
-    role: {
-        type: String,
-        default: 'USER_ROLE',
-        enum: rolesValidos
-    },
-    state: {
-        type: Boolean,
-        default: true
-    },
-    google: {
-        type: Boolean,
-        default: false
-    },
-    facebook: {
-        type: Boolean,
-        default: false
-    }
+  name: {
+    type: String,
+    required: [true, "El name es necesario"]
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "El correo es necesario"]
+  },
+  password: {
+    type: String,
+    required: [true, "La contraseña es obligatoria"]
+  },
+  phone: {
+    type: String,
+    requiered: [true, "El telefono es requerido"]
+  },
+  img: {
+    type: String,
+    required: false
+  },
+  role: {
+    type: String,
+    default: "USER_ROLE",
+    enum: rolesValidos
+  },
+  state: {
+    type: Boolean,
+    default: true
+  },
+  google: {
+    type: Boolean,
+    default: false
+  },
+  facebook: {
+    type: Boolean,
+    default: false
+  },
+  favoriteProducts: [{ type: Schema.Types.ObjectId, ref: "product" }],
+  interestCategories: [{ type: Schema.Types.ObjectId, ref: "category" }]
 });
 
-
 usuarioSchema.methods.toJSON = function() {
+  let user = this;
+  let userObject = user.toObject();
+  delete userObject.password;
 
-    let user = this;
-    let userObject = user.toObject();
-    delete userObject.password;
+  return userObject;
+};
 
-    return userObject;
-}
+usuarioSchema.plugin(uniqueValidator, { message: "{PATH} debe de ser único" });
 
-
-usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
-
-
-module.exports = mongoose.model('User', usuarioSchema);
+module.exports = mongoose.model("User", usuarioSchema);
