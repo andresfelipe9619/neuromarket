17d7565a4f1acb09b592476916e2129dc0fdbcef
Andres Suarez <andresfelipe9619@gmail.com>
2019-09-11 20:16:48 -0500

    Add new configurtations

--- client/src/features/cart/styles.js
+++ client/src/features/cart/styles.js
@@ -3,6 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => ({
   root: {
     textAlign: "center"
+  },
+  sticky: {
+    position: "sticky"
   }
 }));
 export default useStyles;
--- server/seeder/data/categories/index.js
+++ server/seeder/data/categories/index.js
@@ -1,31 +1,25 @@
 module.exports = [
   {
-    _id: 10101,
     name: "Smartphones",
     description: ""
   },
   {
-    _id: 10102,
     name: "Pc & Laptops",
     description: ""
   },
   {
-    _id: 10103,
     name: "TVs",
     description: ""
   },
   {
-    _id: 10104,
     name: "Audio",
     description: ""
   },
   {
-    _id: 10105,
     name: "Video Games",
     description: ""
   },
   {
-    _id: 10106,
     name: "Cameras",
     description: ""
   }
